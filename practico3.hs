eee988bd566561038c759ad7aac5e2475176406f
franpeliciotti <franpelliciotti00@gmail.com>
2024-09-28 13:06:24 -0300

    practico3.hs añadido

---
+++ practico3.hs
@@ -0,0 +1,36 @@
+--Actividad 1:
+hd :: [a] -> a
+hd [] = error "Lista vacía"
+hd (x:xs) = x
+
+tl :: [a] -> [a]
+tl [] = error "Lista vacía"
+tl (x:xs) = xs
+
+lst :: [a] -> a
+lst [] = error "Lista vacía"
+lst [x] = x
+lst (x:xs) = lst xs
+-- lst xs = hd(reverse xs)
+
+init2 :: [a] -> [a]
+init2 [] = []
+init2 [x] = []
+init2 (x:xs) = x:(init2 xs)
+
+--Actividad 2:
+reversa :: [a] -> [a]
+reversa [] = []
+--reversa [x] = [x] --Redundante, puede encajar en el caso siguiente (porque 3 = 3:[])
+reversa (x:xs) = (reversa xs) ++ (x:[])
+
+--Actividad 3:
+--iguales :: Eq a => [a] -> [a] -> Bool
+--iguales [] [] = True
+--iguales [x] [x] = True
+--iguales (x:xs) (y:ys)
+
+esPrimo :: Int -> Bool
+esPrimo x = listaDivisores x == [1, x]
+listaDivisores :: Int -> [Int]
+listaDivisores x = [n | n<-[1..x], x `mod` n == 0]
